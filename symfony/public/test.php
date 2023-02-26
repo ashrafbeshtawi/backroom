bac3a5f8c4f887a5cc6a194b7d6f10a36dc89fe8
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2023-02-26 14:34:19 +0100

    problem detected env-variables not passed in postgresql

--- symfony/public/test.php
+++ symfony/public/test.php
@@ -1,2 +1,13 @@
 <?php
-echo 'reached';
+$host = 'postgresql';
+$port = '5432';
+$dbname = 'app';
+$username = 'admin';
+$password = 'example';
+$connectionString = "host=$host port=$port dbname=$dbname user=$username password=$password";
+$conn = pg_connect($connectionString);
+if(!$conn) {
+    echo "Error : Unable to open database\n";
+} else {
+    echo "Opened database successfully\n";
+}
