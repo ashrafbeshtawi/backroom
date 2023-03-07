554d338331d1b1d81d8d3b38ad19121aa596bffa
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2023-03-07 17:22:15 +0100

    create users table

--- dbmigraion/2023Q1/V1__create_users_table.sql
+++ dbmigraion/2023Q1/V1__create_users_table.sql
@@ -1,7 +1,14 @@
--- V1__create_table.sql
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+-- Create Users Table
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  phone VARCHAR(20),
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW(),
+  active BOOLEAN NOT NULL DEFAULT true,
+  deleted BOOLEAN NOT NULL DEFAULT false,
+  blocked BOOLEAN NOT NULL DEFAULT false
 );
