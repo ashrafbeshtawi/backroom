11344e6c61911d87c9eedc325283815630adfd8c
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2023-03-07 02:47:10 +0100

    running migrations

---
+++ flyway/sql/V2/V2__create_customers_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS customers2 (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
---
+++ flyway/sql/V3/V3__create_customers_table.sql
@@ -0,0 +1,1 @@
+INSERT INTO customers2 VALUES (1, 'ashraf', 'ashe@gmail.com');
