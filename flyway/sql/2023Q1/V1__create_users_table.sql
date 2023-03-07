149d835cba0ae8790f5b2caaa9e4401b1dc5aaf6
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2023-03-07 19:21:59 +0100

    dc compose optimieren

--- flyway/sql/2023Q1/V1__create_users_table.sql
+++ flyway/sql/2023Q1/V1__create_users_table.sql
@@ -5,10 +5,14 @@ CREATE TABLE users (
   last_name VARCHAR(50) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
-  phone VARCHAR(20),
+  phone_number VARCHAR(30),
+  address VARCHAR(255),
+  city VARCHAR(255),
+  state VARCHAR(255),
+  country VARCHAR(255),
+  postal_code VARCHAR(20),
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW(),
-  active BOOLEAN NOT NULL DEFAULT true,
+  active BOOLEAN NOT NULL DEFAULT false,
   deleted BOOLEAN NOT NULL DEFAULT false,
-  blocked BOOLEAN NOT NULL DEFAULT false
 );
