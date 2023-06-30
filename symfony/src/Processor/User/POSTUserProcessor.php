8851cc3c70a019cdc1b1d5216f91c0d1fafd8525
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2023-06-30 17:30:43 +0200

    Adding mail catcher

--- symfony/src/Processor/User/POSTUserProcessor.php
+++ symfony/src/Processor/User/POSTUserProcessor.php
@@ -6,13 +6,16 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class POSTUserProcessor implements ProcessorInterface
 {
   public function __construct(
     private readonly UserPasswordHasherInterface $passwordHasher,
-    private readonly EntityManagerInterface $entityManager
+    private readonly EntityManagerInterface $entityManager,
+    private MailerInterface $mailer
   ) {
   }
 
@@ -27,6 +30,18 @@ final class POSTUserProcessor implements ProcessorInterface
     $data->setRoles(['ROLE_USER']);
     $this->entityManager->persist($data);
     $this->entityManager->flush();
+    $email = (new Email())
+      ->from('hello@example.com')
+      ->to('you@example.com')
+      //->cc('cc@example.com')
+      //->bcc('bcc@example.com')
+      //->replyTo('fabien@example.com')
+      //->priority(Email::PRIORITY_HIGH)
+      ->subject('Time for Symfony Mailer!')
+      ->text('Sending emails is fun again!')
+      ->html('<p>See Twig integration for better HTML integration!</p>');
+
+    $this->mailer->send($email);
 
   }
 }
