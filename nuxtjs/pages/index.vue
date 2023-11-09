f8c3ca21557f3744425597536337acf0168602d9
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2023-11-09 13:01:08 +0100

    dummy landing

--- nuxtjs/pages/index.vue
+++ nuxtjs/pages/index.vue
@@ -1,19 +1,15 @@
 <template>
-  <div>
+  <v-navigation-drawer app v-model="sidebarOpen">
+    <!-- Sidebar content goes here -->
+  </v-navigation-drawer>
     <!-- Header Section -->
-    <v-app>
-      <v-navigation-drawer app>
-        <!-- Sidebar content goes here -->
-      </v-navigation-drawer>
-
-      <v-app-bar app color="primary">
-        <v-app-bar-nav-icon @click="toggleSidebar" />
-        <v-toolbar-title>Freelancer Portfolio</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn outlined>Sign Up</v-btn>
-        <v-btn text>Login</v-btn>
-      </v-app-bar>
-    </v-app>
+    <v-app-bar app color="primary">
+      <v-app-bar-nav-icon @click="toggleSidebar" />
+      <v-toolbar-title>Freelancer Portfolio</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn >Sign Up</v-btn>
+      <v-btn >Login</v-btn>
+    </v-app-bar>
 
     <!-- Main Content Section -->
     <v-main>
@@ -21,50 +17,59 @@
         <!-- Hero Section -->
         <v-row align="center">
           <v-col cols="12">
-            <h1>Welcome to Your Freelancer Portfolio</h1>
-            <p>Showcase your skills and work to potential clients.</p>
+            <h1 class="display-3 text-center">Welcome to Your Freelancer Portfolio</h1>
+            <p class="text-center">Showcase your skills and work to potential clients.</p>
             <v-btn color="primary" large>Get Started</v-btn>
           </v-col>
         </v-row>
 
         <!-- Features Section -->
         <v-row>
-          <v-col cols="12" sm="6" md="4">
-            <v-card>
-              <v-card-title>Feature 1</v-card-title>
+          <v-col cols="12" md="4">
+            <v-card class="text-center">
+              <v-card-title class="headline">Feature 1</v-card-title>
               <v-card-text>Highlight your skills and expertise.</v-card-text>
             </v-card>
           </v-col>
-          <v-col cols="12" sm="6" md="4">
-            <v-card>
-              <v-card-title>Feature 2</v-card-title>
+          <v-col cols="12" md="4">
+            <v-card class="text-center">
+              <v-card-title class="headline">Feature 2</v-card-title>
               <v-card-text>Showcase your work with ease.</v-card-text>
             </v-card>
           </v-col>
-          <v-col cols="12" sm="6" md="4">
-            <v-card>
-              <v-card-title>Feature 3</v-card-title>
+          <v-col cols="12" md="4">
+            <v-card class="text-center">
+              <v-card-title class="headline">Feature 3</v-card-title>
               <v-card-text>Connect with potential clients.</v-card-text>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
     </v-main>
+
     <!-- Footer Section -->
-    <v-footer app>
+    <v-footer>
       <v-container>
-        <!-- Footer content goes here -->
+        <v-row>
+          <v-col cols="12" class="text-center">
+            &copy; 2023 Freelancer Portfolio. All rights reserved.
+          </v-col>
+        </v-row>
       </v-container>
     </v-footer>
-  </div>
 </template>
+
 <script>
 export default {
+  data() {
+    return {
+      sidebarOpen: false,
+    };
+  },
   methods: {
     toggleSidebar() {
-      // Implement the sidebar toggle logic here
+      this.sidebarOpen = !this.sidebarOpen;
     },
   },
 };
 </script>
-
