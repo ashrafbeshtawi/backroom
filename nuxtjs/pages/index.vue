3654f891440b51ff57074094c424e95194120bfd
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2023-11-19 23:03:36 +0100

    add veutify theme

--- nuxtjs/pages/index.vue
+++ nuxtjs/pages/index.vue
@@ -13,38 +13,40 @@
 
     <!-- Main Content Section -->
     <v-main>
-      <v-container>
-        <!-- Hero Section -->
-        <v-row align="center">
-          <v-col cols="12">
-            <h1 class="display-3 text-center">Welcome to Your Freelancer Portfolio</h1>
-            <p class="text-center">Showcase your skills and work to potential clients.</p>
-            <v-btn color="primary" large>Get Started</v-btn>
-          </v-col>
-        </v-row>
+      <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+        <v-container>
+          <!-- Hero Section -->
+          <v-row align="center">
+            <v-col cols="12">
+              <h1 class="display-3 text-center">Welcome to Your Freelancer Portfolio</h1>
+              <p class="text-center">Showcase your skills and work to potential clients.</p>
+              <v-btn color="primary" large>Get Started</v-btn>
+            </v-col>
+          </v-row>
 
-        <!-- Features Section -->
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-card class="text-center">
-              <v-card-title class="headline">Feature 1</v-card-title>
-              <v-card-text>Highlight your skills and expertise.</v-card-text>
-            </v-card>
-          </v-col>
-          <v-col cols="12" md="4">
-            <v-card class="text-center">
-              <v-card-title class="headline">Feature 2</v-card-title>
-              <v-card-text>Showcase your work with ease.</v-card-text>
-            </v-card>
-          </v-col>
-          <v-col cols="12" md="4">
-            <v-card class="text-center">
-              <v-card-title class="headline">Feature 3</v-card-title>
-              <v-card-text>Connect with potential clients.</v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+          <!-- Features Section -->
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-card class="text-center">
+                <v-card-title class="headline">Feature 1</v-card-title>
+                <v-card-text>Highlight your skills and expertise.</v-card-text>
+              </v-card>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-card class="text-center">
+                <v-card-title class="headline">Feature 2</v-card-title>
+                <v-card-text>Showcase your work with ease.</v-card-text>
+              </v-card>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-card class="text-center">
+                <v-card-title class="headline">Feature 3</v-card-title>
+                <v-card-text>Connect with potential clients.</v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-parallax>
     </v-main>
 
     <!-- Footer Section -->
--- nuxtjs/plugins/veutify.ts
+++ nuxtjs/plugins/veutify.ts
@@ -2,9 +2,27 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    background: '#00ADB5',
+    surface: '#222831',
+    primary: '#222831',
+    secondary: '#393E46',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+}
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    theme: {
+      defaultTheme: 'myCustomLightTheme',
+      themes: {
+        myCustomLightTheme,
+      },
+    },
   })
   app.vueApp.use(vuetify)
 })
