9d7e4b899544a933cf6d4caadd2e8ac44290bc74
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2023-11-28 14:34:46 +0100

    adding planet and card to fe

---
+++ nuxtjs/pages/[id].vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <!-- Hero Section -->
+    <v-row align="center">
+      <v-col cols="12">
+        <h1 class="display-3 text-center">Welcome to Your Freelancer Portfolio</h1>
+        <p class="text-center">Showcase your skills and work to potential clients.</p>
+        <v-btn color="primary" large>Get Started</v-btn>
+      </v-col>
+    </v-row>
+
+    <!-- Features Section -->
+    <v-row>
+      <v-col cols="12" md="4">
+        <v-card class="text-center">
+          <v-card-title class="headline">Feature 1</v-card-title>
+          <v-card-text>Highlight your skills and expertise.</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-card class="text-center">
+          <v-card-title class="headline">Feature 2</v-card-title>
+          <v-card-text>Showcase your work with ease.</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-card class="text-center">
+          <v-card-title class="headline">Feature 3</v-card-title>
+          <v-card-text>Connect with potential clients.</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  <!-- Footer Section -->
+  <v-footer>
+    <v-container>
+      <v-row>
+        <v-col cols="12" class="text-center">
+          &copy; 2023 Freelancer Portfolio. All rights reserved.
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sidebarOpen: false,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.sidebarOpen = !this.sidebarOpen;
+    },
+  },
+};
+</script>
--- nuxtjs/pages/index.vue
+++ nuxtjs/pages/index.vue
@@ -1,52 +1,36 @@
 <template>
   <v-navigation-drawer app v-model="sidebarOpen">
-    <!-- Sidebar content goes here -->
+    <v-list-item title="XFolio" subtitle="Portfolio on Steroids"></v-list-item>
+    <v-divider></v-divider>
+    <v-list-item  title="Home"></v-list-item>
+    <v-list-item  title="Demo"></v-list-item>
+    <v-list-item  title="Why XFolio"></v-list-item>
   </v-navigation-drawer>
     <!-- Header Section -->
     <v-app-bar app color="primary">
       <v-app-bar-nav-icon @click="toggleSidebar" />
-      <v-toolbar-title>Freelancer Portfolio</v-toolbar-title>
+      <v-toolbar-title>XFolio</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn >Home</v-btn>
+      <v-btn >Demo</v-btn>
+      <v-btn >Why XFolio</v-btn>
       <v-btn >Sign Up</v-btn>
       <v-btn >Login</v-btn>
     </v-app-bar>
 
     <!-- Main Content Section -->
-    <v-main>
-      <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
-        <v-container>
-          <!-- Hero Section -->
-          <v-row align="center">
-            <v-col cols="12">
-              <h1 class="display-3 text-center">Welcome to Your Freelancer Portfolio</h1>
-              <p class="text-center">Showcase your skills and work to potential clients.</p>
-              <v-btn color="primary" large>Get Started</v-btn>
-            </v-col>
-          </v-row>
+    <v-main class="wrapper">
 
-          <!-- Features Section -->
-          <v-row>
-            <v-col cols="12" md="4">
-              <v-card class="text-center">
-                <v-card-title class="headline">Feature 1</v-card-title>
-                <v-card-text>Highlight your skills and expertise.</v-card-text>
-              </v-card>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-card class="text-center">
-                <v-card-title class="headline">Feature 2</v-card-title>
-                <v-card-text>Showcase your work with ease.</v-card-text>
-              </v-card>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-card class="text-center">
-                <v-card-title class="headline">Feature 3</v-card-title>
-                <v-card-text>Connect with potential clients.</v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-parallax>
+      <v-container>
+        <v-row class="mt-5">
+          <v-col>
+            <v-card title="XFolio" text="lorem ipsum" variant="tonal"></v-card>
+          </v-col>
+          <v-col class="flex justify-space-around">
+            <img class="planet" src="planet1.png">
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
 
     <!-- Footer Section -->
@@ -54,13 +38,32 @@
       <v-container>
         <v-row>
           <v-col cols="12" class="text-center">
-            &copy; 2023 Freelancer Portfolio. All rights reserved.
+            &copy; 2023 XFolio. All rights reserved.
           </v-col>
         </v-row>
       </v-container>
     </v-footer>
 </template>
+<style scoped>
+.wrapper {
+  height: 95%;
+  background-image: url('background.jpg');
+  background-repeat: repeat;
+}
+.planet {
+  width: 400px;
+  animation: bounce 5s infinite;
+}
 
+@keyframes bounce {
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+}
+</style>
 <script>
 export default {
   data() {
