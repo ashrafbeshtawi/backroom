44d93bdb1d4cd4e9ad93d9aa77c13b4bc630a619
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2023-12-16 11:30:39 +0100

    cs

--- nuxtjs/pages/index.vue
+++ nuxtjs/pages/index.vue
@@ -2,42 +2,42 @@
   <v-navigation-drawer app v-model="sidebarOpen">
     <v-list-item title="XFolio" subtitle="Portfolio on Steroids"></v-list-item>
     <v-divider></v-divider>
-    <v-list-item  title="Home"></v-list-item>
-    <v-list-item  title="Demo"></v-list-item>
-    <v-list-item  title="Why XFolio"></v-list-item>
+    <v-list-item title="Home"></v-list-item>
+    <v-list-item title="Demo"></v-list-item>
+    <v-list-item title="Why XFolio"></v-list-item>
   </v-navigation-drawer>
-    <!-- Header Section -->
-    <v-app-bar app color="primary">
-      <v-app-bar-nav-icon @click="toggleSidebar" />
-      <v-toolbar-title>XFolio</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn >Home</v-btn>
-      <v-btn >Demo</v-btn>
-      <v-btn >Why XFolio</v-btn>
-      <v-btn >Sign Up</v-btn>
-      <v-btn >Login</v-btn>
-    </v-app-bar>
+  <!-- Header Section -->
+  <v-app-bar app color="primary">
+    <v-app-bar-nav-icon @click="toggleSidebar"/>
+    <v-toolbar-title>XFolio</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn>Home</v-btn>
+    <v-btn>Demo</v-btn>
+    <v-btn>Why XFolio</v-btn>
+    <v-btn>Sign Up</v-btn>
+    <v-btn>Login</v-btn>
+  </v-app-bar>
 
-    <v-main class="wrapper">
-      <v-row class="mt-5 h-screen">
-        <v-col class="flex justify-space-around align-center">
-          <div class="title text-5xl">
-            {{title}}
-          </div>
+  <v-main class="wrapper">
+    <v-row class="mt-5 h-screen">
+      <v-col class="flex justify-space-around align-center">
+        <div class="title text-5xl">
+          {{ title }}
+        </div>
+      </v-col>
+    </v-row>
+  </v-main>
+
+  <!-- Footer Section -->
+  <v-footer>
+    <v-container>
+      <v-row>
+        <v-col cols="12" class="text-center">
+          &copy; 2023 XFolio. All rights reserved.
         </v-col>
       </v-row>
-    </v-main>
-
-    <!-- Footer Section -->
-    <v-footer>
-      <v-container>
-        <v-row>
-          <v-col cols="12" class="text-center">
-            &copy; 2023 XFolio. All rights reserved.
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-footer>
+    </v-container>
+  </v-footer>
 </template>
 <style scoped>
 .wrapper {
@@ -45,6 +45,7 @@
   background-image: url('background.jpg');
   background-size: cover;
 }
+
 .planet {
   width: 400px;
   animation: bounce 5s;
@@ -85,14 +86,14 @@ export default {
         {title: 'Second one', description: 'Very cool app'},
         {title: 'Third one', description: 'Very cool app'},
       ];
-      const timeline = anime.timeline({ loop: true });
+      const timeline = anime.timeline({loop: true});
       let j = -1;
 
       timeline
         .add({
           targets: '.title',
-          scale: [4,1],
-          opacity: [0,1],
+          scale: [4, 1],
+          opacity: [0, 1],
           translateZ: 0,
           easing: "easeOutExpo",
           duration: 2000,
@@ -110,7 +111,6 @@ export default {
           easing: "easeOutExpo",
         })
     }
-
   },
 };
 </script>
